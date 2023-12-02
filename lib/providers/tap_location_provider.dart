9326787fe463a21dc1750c347733e533ddc25f7e
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-02 17:31:15 +0100

    feat(providers): add chooseRandomTap() function & ask for offset in moveCoordinates()

--- lib/providers/tap_location_provider.dart
+++ lib/providers/tap_location_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
 import 'package:random_chooser/models/tap.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -8,12 +11,12 @@ class TapCoordinatesNotifier extends StateNotifier<List<Tap>> {
     state = <Tap>[...state, tap];
   }
 
-  void moveCoordinates(Tap tap) {
+  void moveCoordinates(Tap tap, Offset offset) {
     int index = state.indexWhere((element) => element.id == tap.id);
     if (index != -1) {
       state = [
         ...state.sublist(0, index),
-        tap,
+        Tap(id: tap.id, offset: offset, isChosen: tap.isChosen),
         ...state.sublist(index + 1),
       ];
     }
@@ -21,6 +24,17 @@ class TapCoordinatesNotifier extends StateNotifier<List<Tap>> {
 
   void removeCoordinate(Tap tap) {
     state = state.where((element) => element.id != tap.id).toList();
+  }
+
+  void chooseRandomTap() {
+    Random random = Random();
+    int randomIndex = random.nextInt(state.length);
+    var chosenTap = state[randomIndex];
+    state = [
+      ...state.sublist(0, randomIndex),
+      Tap(id: chosenTap.id, offset: chosenTap.offset, isChosen: true),
+      ...state.sublist(randomIndex + 1),
+    ];
   }
 }
 
