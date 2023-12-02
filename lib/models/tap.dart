3015833b51e18fe6904890b1db52508935461931
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-02 17:26:14 +0100

    feat(models): add isChosen property to Tap model

--- lib/models/tap.dart
+++ lib/models/tap.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Tap {
-  const Tap({required this.id, required this.offset});
+  const Tap({required this.id, required this.offset, this.isChosen = false});
 
   final int id;
   final Offset offset;
+  final bool isChosen;
 }
