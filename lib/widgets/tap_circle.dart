f13679bd23468425dc42c7ac5a828746a9b4c515
Peter Abraham <peterabrahamdev@gmail.com>
2023-12-03 14:49:31 +0100

    style(drawing): adjust circle radius for selected state

--- lib/widgets/tap_circle.dart
+++ lib/widgets/tap_circle.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:random_chooser/models/tap.dart';
 
@@ -10,12 +11,10 @@ class TapCircle extends CustomPainter {
       tap.isChosen
           ? canvas.drawCircle(
               tap.offset,
-              60,
+              70,
               Paint()
                 ..color = Colors.white
-                ..style = PaintingStyle.fill
-                ..strokeWidth = 10,
-            )
+                ..style = PaintingStyle.fill)
           : canvas.drawCircle(
               tap.offset,
               60,
@@ -28,7 +27,6 @@ class TapCircle extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
     return true;
   }
 }
